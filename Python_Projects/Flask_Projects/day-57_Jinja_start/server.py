4abb1b581d91e0225867be449b0d3ed9c92046c2
Oskaras-coder <osafinas@gmail.com>
2024-01-16 20:13:59 +0200

    day-57 completed

--- Python_Projects/Flask_Projects/day-57_Jinja_start/server.py
+++ Python_Projects/Flask_Projects/day-57_Jinja_start/server.py
@@ -1,12 +1,35 @@
 from flask import Flask, render_template
+import random
+import datetime
+import requests
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    random_number = random.randint(1, 10)
+    year_number = datetime.datetime.now().year
+    return render_template("index.html", num=random_number, year=year_number)
 
+
+@app.route("/guess/<string:name>")
+def check_the_name(name):
+    json_url_gender = f"https://api.genderize.io/?name={name}"
+    gender_json_file = requests.get(url=json_url_gender).json()
+    gender = gender_json_file["gender"]
+
+    json_url_age = f"https://api.agify.io?name={name}"
+    age_json_file = requests.get(url=json_url_age).json()
+    age = age_json_file["age"]
+
+    return render_template("guess.html", name=name.capitalize(), gender=gender, age=age)
+
+@app.route("/blog/<num>")
+def get_blog(num):
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    blog_data = requests.get(blog_url).json()
+    return render_template("blog.html", posts=blog_data)
 
 if __name__ == "__main__":
     app.run(port=5001,
